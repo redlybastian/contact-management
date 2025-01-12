a45064415bfa1dc9097c5ec2bb817c88498a2c07
redlybastian <red233786@gmail.com>
2025-01-12 19:11:34 +0530

    "⛑️ create middleware for handling ⛑️"

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+// @Note : error handler middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
