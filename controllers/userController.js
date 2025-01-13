7e3fafbcbf18356afed9effcb3e7ebaf2ced72d2
redlybastian <red233786@gmail.com>
2025-01-13 18:42:53 +0530

    create user models ⚡ and user controller functions🚧

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,58 @@
+const asyncHandler = require("express-async-handler");
+
+const bcrypt = require("bcrypt");
+
+const User = require("../models/userModels");
+
 // @dec Register a user
 // @route POST /api/users/register
 // @access public
+// @Note this function not change/ the error messages because for the throw err is sent multiple response. nodejs not allow for it
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({
-    message: "Register the user",
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    // return res.status(400).json({ error: "All fields are mandatory" });
+
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered");
+  }
+
+  //   Hash password
+  const hashPassword = await bcrypt.hash(password, 10);
+  // console.log("Hash Password : ", hashPassword);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashPassword,
   });
+
+  console.log(`User created ${user}`);
+  if (user) {
+    res.status(201).json({
+      message: "Register the user",
+      _id: user.id,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User data not valid");
+  }
+
+  // res.json({
+  //   message: "Register the user",
+  // });
 });
 
 // @dec User login
 // @route POST /api/users/login
 // @access public
-const userLogin = asyncHandler(async, (req, res) => {
+const userLogin = asyncHandler(async (req, res) => {
   res.json({
     message: "login user",
   });
@@ -19,7 +61,7 @@ const userLogin = asyncHandler(async, (req, res) => {
 // @dec Current user ifo
 // @route GET /api/users/current
 // @access public
-const currentUserInformation = asyncHandler(async, (req, res) => {
+const currentUserInformation = asyncHandler(async (req, res) => {
   res.json({
     message: "current user information",
   });
