5ff7be850c8dc7fa83d8a49171559721001aa4b9
redlybastian <red233786@gmail.com>
2025-01-13 19:49:38 +0530

    🚀create validation middleware🚀

--- middleware/valideteTokenHandler.js
+++ middleware/valideteTokenHandler.js
@@ -1,2 +1,20 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
+
+const validateToken = asyncHandler(async (req, res, next) => {
+  let token;
+  let authHeader = req.header.Authorization || req.header.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        throw new Error("User is not authorized");
+      }
+      console.log(decoded);
+    });
+  }
+});
+
+module.exports = validateToken;
